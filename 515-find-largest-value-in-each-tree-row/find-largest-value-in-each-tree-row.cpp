56720c7a52446e7855e678c53ac9f14ff5570c81
Anurag Verma <anurag1246verma2121@gmail.com>
2026-08-29 15:41:27 +0530

    Time: 0 ms (100.00%) | Memory: 22.7 MB (89.76%) - LeetSync

---
+++ 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.cpp
@@ -0,0 +1,27 @@
+
+class Solution {
+public:
+    vector <int> result;
+
+    void dfs(TreeNode* root , int depth){
+        if(root == NULL)
+        return;
+
+        if(depth == result.size()){
+            result.push_back(root->val);
+        }
+        else
+        {
+            result[depth] = max(result[depth], root->val);
+        }
+
+        dfs(root->left,depth+1);
+        dfs(root->right,depth+1);
+    }
+
+    vector<int> largestValues(TreeNode* root) {
+        dfs(root,0);
+
+        return result;
+    }
+};
