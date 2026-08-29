09c2dd7c5f77a58b1c4c0a87274775972a2b0106
Anurag Verma <anurag1246verma2121@gmail.com>
2026-08-30 02:38:00 +0530

    Time: 5 ms (72.38%) | Memory: 15.8 MB (29.68%) - LeetSync

--- 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.cpp
+++ 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.cpp
@@ -1,50 +1,32 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    int res ;
-    int count;
-    int countSum(TreeNode* root){
-        if(root == NULL)
-        return 0;
 
-        count++;
+    int result;
 
-        int sum = root->val; 
+    pair<int, int> Solve(TreeNode* root){
+        if(!root){
+            return {0,0};
+        }
 
-        sum+=countSum(root->left);
-        sum+=countSum(root->right);
+        auto P1 = Solve(root->left);
+        auto P2 = Solve(root->right);
 
-        return sum;
-    }
+        int totalSum = P1.first + P2.first + root->val;
+        int totalCount = P1.second + P2.second + 1;
 
-    void solve(TreeNode* root){
-        if(root == NULL)
-        return;
+        int avg = totalSum / totalCount;
 
-        count = 0;
-        int sum = countSum(root);
+        if(avg == root->val)
+            result++;
 
-        if(root->val == (sum/count))
-        res++;
-
-        solve(root->left);
-        solve(root->right);
+        return {totalSum , totalCount};
     }
 
     int averageOfSubtree(TreeNode* root) {
-        res = 0;
-        solve(root);
+        result = 0;
 
-        return res;
+        Solve(root);
+
+        return result;
     }
 };
