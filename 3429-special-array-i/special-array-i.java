98c620552cfdeb0423288a2505e4b9ec7b5686b6
Anurag Verma <anurag1246verma2121@gmail.com>
2026-09-09 18:52:19 +0530

    Time: 1 ms (59.00%) | Memory: 44.9 MB (86.52%) - LeetSync

--- 3429-special-array-i/special-array-i.java
+++ 3429-special-array-i/special-array-i.java
@@ -9,9 +9,7 @@ class Solution {
         int odd = 0;
 
         for(int i = 1; i<n ;i++){
-            if(nums[i] % 2 == 0 && nums[i-1] % 2 == 0)
-            return false;
-            if(nums[i] % 2 == 1 && nums[i-1] % 2 == 1)
+            if(nums[i] % 2 == nums[i-1] % 2)
             return false;
         }
         return true;
