a6aaf63b955bed0a9bc57abe7df64543d717c055
Anurag Verma <anurag1246verma2121@gmail.com>
2026-09-11 02:22:24 +0530

    Time: 3 ms (46.34%) | Memory: 46.7 MB (9.28%) - LeetSync

---
+++ 2756-buy-two-chocolates/buy-two-chocolates.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int buyChoco(int[] prices, int money) {
+        Arrays.sort(prices);
+
+        int sum = prices[0] + prices[1];
+
+        return sum > money ? money : (money - sum); 
+    }
+}
