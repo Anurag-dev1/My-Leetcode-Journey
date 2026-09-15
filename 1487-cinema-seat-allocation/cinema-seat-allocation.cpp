a77b70d11408988e2ba2786f62fee0fafd0a60a8
Anurag Verma <anurag1246verma2121@gmail.com>
2026-09-15 20:15:16 +0530

    Time: 19 ms (72.98%) | Memory: 60.2 MB (55.27%) - LeetSync

--- 1487-cinema-seat-allocation/cinema-seat-allocation.cpp
+++ 1487-cinema-seat-allocation/cinema-seat-allocation.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
-        unordered_map<int , unordered_set<int>> mp;
+        unordered_map<int , int> mp;
 
         for(auto & reservedSeat : reservedSeats){
             int row = reservedSeat[0];
-            int col = reservedSeat[1];
+            int seat = reservedSeat[1];
 
-            mp[row].insert(col);
+            mp[row] |= (1 << seat);
         }
 
         int res = (n - mp.size())*2;
 
-        for(auto & [row , bookedSeats] : mp){
+        int maskA = (1 << 2) | (1 << 3) | (1 << 4) | (1 << 5);
+        int maskB = (1 << 7) | (1 << 6) | (1 << 4) | (1 << 5);
+        int maskC = (1 << 8) | (1 << 9) | (1 << 6) | (1 << 7);
 
-            auto isAvailable = [&](int seat){
-                return bookedSeats.find(seat) == bookedSeats.end();
-            };
+        for(auto & [row , bookedSeatsMask] : mp){
 
-            bool GroupA = isAvailable(2) &&isAvailable(3) &&isAvailable(5) && isAvailable(4);
-            bool GroupB = isAvailable(4) &&isAvailable(5) &&isAvailable(6) && isAvailable(7);
-            bool GroupC = isAvailable(8) &&isAvailable(9) &&isAvailable(6) && isAvailable(7);
+            bool GroupA = (bookedSeatsMask & maskA) == 0;
+            bool GroupB = (bookedSeatsMask & maskB) == 0;
+            bool GroupC = (bookedSeatsMask & maskC) == 0;
 
             if(GroupA && GroupC)
             res += 2;
