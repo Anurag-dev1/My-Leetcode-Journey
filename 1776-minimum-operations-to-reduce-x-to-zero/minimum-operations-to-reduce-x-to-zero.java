9813b03fce1c43b861edd0fc517370ed957e3b50
Anurag Verma <anurag1246verma2121@gmail.com>
2026-09-24 00:53:39 +0530

    Time: 4 ms (98.07%) | Memory: 102.2 MB (44.57%) - LeetSync

---
+++ 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int minOperations(int[] nums, int x) {
+        int n = nums.length;
+
+        int total = 0;
+
+        for(int i : nums)
+        total += i;
+
+        int target = total - x;
+
+        if(target == 0)
+        return n;
+
+        int maxLen = -1;
+        int left = 0;
+        int sum = 0;
+
+        for(int right = 0 ; right < n ; right++){
+            sum += nums[right];
+
+            while(left <= right && sum > target){
+                sum -= nums[left];
+                left++;
+            }
+
+            if(sum == target)
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+
+        if(maxLen == -1)
+        return -1;
+
+        return n-maxLen;
+    }
+}
